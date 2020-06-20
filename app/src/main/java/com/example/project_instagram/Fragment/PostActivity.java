c36f802339920a04e84188b97fd0b48f5dcdfe51
hemuhemanth658 <59231711+hemuhemanth658@users.noreply.github.com>
2020-06-20 13:25:19 +0530

    commit 2 (Bottom AppBar Fragments)

---
+++ app/src/main/java/com/example/project_instagram/Fragment/PostActivity.java
@@ -0,0 +1,16 @@
+package com.example.project_instagram.Fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.project_instagram.R;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
